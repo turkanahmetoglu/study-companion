2b96c7360fb2d0c2e70037a832d43d5c8ea42600
Türkan Begüm Ahmetoğlu <turkan.ahmetoglu@sabanciuniv.edu>
2026-09-22 20:05:41 +0300

    Initial commit

---
+++ src/test/java/com/turkan/study_companion/StudyCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkan.study_companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
